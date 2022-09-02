f71b6b9c0428b631dd1cd674e9cfb2e264b26717
Balázs Sághy <balazs.saghy@prezi.com>
2022-09-02 10:03:23 +0200

    feat: adding express server

--- index.ts
+++ index.ts
@@ -1,15 +1,28 @@
-import Arena from "./Arena.js";
-import Mage from "./hero-types/Mage.js";
-import Warrior from "./hero-types/Warrior.js";
-import BattleAxe from "./weapons/BattleAxe.js";
-import Sword from "./weapons/Sword.js";
+// import Arena from "./Arena.js";
+// import Mage from "./hero-types/Mage.js";
+// import Warrior from "./hero-types/Warrior.js";
+// import BattleAxe from "./weapons/BattleAxe.js";
+// import Sword from "./weapons/Sword.js";
 
-const warrior = new Warrior("Mars");
-warrior.equipWeapon(new BattleAxe());
+// const warrior = new Warrior("Mars");
+// warrior.equipWeapon(new BattleAxe());
 
-const mage = new Mage("Jupiter");
-mage.equipWeapon(new Sword());
+// const mage = new Mage("Jupiter");
+// mage.equipWeapon(new Sword());
 
-const arena = new Arena(warrior, mage);
+// const arena = new Arena(warrior, mage);
 
-arena.fight();
+// arena.fight();
+
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
